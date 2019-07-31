123a2766d8633c3fbb3123ba7c22d82c28d16e17
iSmash <nemeth.richo@gmail.com>
2019-07-31 17:02:55 +0100

    setup test for signing_user_in_spec.rb#Sign in successfully. should be passed already

---
+++ spec/features/users/signing_users_in_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature "User sign_in" do
+
+  before do
+  	@john = User.create!(first_name: "John", last_name: "Doe",email: "john@example.com", password: "password")
+  end
+
+  scenario "Sign in successfully" do 
+
+  	visit "/"
+
+  	click_link "Sign In"
+  	fill_in "Email", with: @john.email
+    fill_in "Password", with: @john.password
+    click_button "Log in"
+
+    expect(page).to have_content("Signed in successfully.")
+    expect(page).to have_content("Welcome:#{@john.first_name}")
+    expect(page).to have_link("Sign Out")
+    expect(page).to_not have_link("Sign up")
+    expect(page).to_not have_link("Sign in")
+
+  end
+end
