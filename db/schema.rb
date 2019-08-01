a404739d4594d219a1c875d2f117509eec77ea96
iSmash <nemeth.richo@gmail.com>
2019-08-01 13:24:54 +0100

    address model has been created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_31_152208) do
+ActiveRecord::Schema.define(version: 2019_08_01_122027) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "postcode"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
