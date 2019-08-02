66609b03a1747dd3f9acf8c6530ac889a957f464
Adam Nemeth <nemeth.richo@gmail.com>
2019-08-02 09:47:10 +0100

    test for add_card_spec.rb#With invalid details make the test passed with validations

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,7 @@
 class Card < ApplicationRecord
   belongs_to :user
+
+  validates :last_four_digits, presence: true
+  validates :expiry_date, presence: true
+  validates_length_of :last_four_digits, is: 4,  message: "Number must be 4 digit long"
 end
--- spec/features/cards/add_card_spec.rb
+++ spec/features/cards/add_card_spec.rb
@@ -25,7 +25,24 @@ RSpec.feature "Add card for user" do
     expect(page).not_to have_link('Add Card')
     expect(page).to have_content("Your card's last four digits: 1234")
 
+  end
 
+    scenario "With valid details" do 
+
+    login_as(@john)
+
+    visit "/"
+
+    expect(page).to have_content("Your card is not registered yet")
+
+    click_link('Add Card')
+
+    fill_in "card_last_four_digits", with: 12121212121
+    fill_in "card_expiry_date", with: ""
+
+    click_button "Save Card"
+
+    expect(page).to have_content("Card has not been added")
 
   end
 
