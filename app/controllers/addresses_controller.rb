9d2bc03046183ae76db9a51e795cfe2383f77c2c
Adam Nemeth <nemeth.richo@gmail.com>
2019-08-02 10:35:22 +0100

    addresses_controller#new and create action has been setup

---
+++ app/controllers/addresses_controller.rb
@@ -0,0 +1,18 @@
+class AddressesController < ApplicationController
+
+  def new
+    @address = Address.new
+  end
+
+  def create
+    @address = Card.new(params.require(:address).permit(:postcode,:user_id))
+    if @address.save
+      flash[:notce] = "Address has been added"
+      redirect_to dashboards_path
+    else
+      flash[:alarm] = "Address has not been added"
+      redirect_to new_addres_path
+    end
+  end
+
+end
